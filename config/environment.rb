8cd0212c0d1b3a00b807687b17f1eda44b29860c
jmw0426 <jmw0426@hotmail.com>
2020-09-11 13:41:42 -0500

    Final formatting.

---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+require 'pry'
+require 'httparty'
+
+require_relative "../lib/currency_exchange/version"
+require_relative "../lib/api_manager"
+require_relative "../lib/currency"
+require_relative "../lib/cli"
+
+module CurrencyExchange
+  
+end
